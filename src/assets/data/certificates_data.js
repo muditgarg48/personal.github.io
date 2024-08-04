99d34dd5e67f0ae752adf1465f0eb276553592ad
muditgarg48 <muditgarg48@gmail.com>
2024-08-05 03:42:12 +0530

    Implementing the Certificate section of the page

---
+++ src/assets/data/certificates_data.js
@@ -0,0 +1,237 @@
+const certificates_data = [
+    {
+        "name":"JP Morgan Chase Software Engineering Virtual Experience",
+        "file_name": "JPMC Virtual Internship.pdf",
+        "cat": [
+            "Virtual Internship"
+        ],
+        "issuing_auth": [
+            "The Forage",
+            "JP Morgan Chase"
+        ],
+        "date": "2020-05-04",
+        "cert_id": "DKNBFdSY4L6QNFWBP",
+        "other_keywords":[]
+    },
+    {
+        "name":"Technical Support Fundamentals",
+        "file_name": "Technical Support Fundamentals by Google.pdf",
+        "cat": [
+            "Online Course"
+        ],
+        "issuing_auth": [
+            "Coursera",
+            "Google"
+        ],
+        "date": "2021-12-31",
+        "cert_id": "KD7DWLWUNL35",
+        "other_keywords":[]
+    },
+    {
+        "name":"HackerRank Problem Solving (Basic)",
+        "file_name": "HackerRank Problem Solving (Basic).pdf",
+        "cat": [
+            "Online Practise"
+        ],
+        "issuing_auth": [
+            "HackerRank"
+        ],
+        "date": "2020-06-10",
+        "cert_id": "AD0CE4699EEB",
+        "other_keywords":[]
+    },
+    {
+        "name":"Debugging C Code",
+        "file_name": "Debugging C Code.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-10",
+        "cert_id": "AQ81m1S1ZnbjdhQUs5hJ6tFFEgrm",
+        "other_keywords":[]
+    },
+    {
+        "name":"Learning C",
+        "file_name": "Learning C - with exam result.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-10",
+        "cert_id": "ASapop6sx8h2hFaIbJ9CDYGS4FJd",
+        "other_keywords":[]
+    },
+    {
+        "name":"Learning GitLab",
+        "file_name": "Learning GitLab.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-03",
+        "cert_id": "AVBg0SwHbBXjiK8BOeM9Hl6ZTYYq",
+        "other_keywords":[]
+    },
+    {
+        "name":"Learning Visual Studio Code",
+        "file_name": "Learning Visual Studio Code.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-09",
+        "cert_id": "AWEY_V2Kn0NbEDR3i2qIZOZHzl7Y",
+        "other_keywords":[]
+    },
+    {
+        "name":"Programming Foundations: Version Control with Git",
+        "file_name": "Programming Foundations of VCS with Git.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-08",
+        "cert_id": "ASO9MY_jci0te2lL8NgOJLtpy7k2",
+        "other_keywords":[]
+    },
+    {
+        "name":"Visual Studio Code Productivity Tips",
+        "file_name": "Visual Studio Code Productivity Tips.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "College"
+        ],
+        "issuing_auth": [
+            "Linkedin Learning"
+        ],
+        "date": "2022-02-09",
+        "cert_id": "Ad0PZpKnjuBqz2vtPUxFVztviI1h",
+        "other_keywords":[]
+    },
+    // {
+    //     "name":"DATA STRUCTURES-UCS301-2021ODDSEM",
+    //     "file_name": "UCS301_Data_Structures_Completion_Certificate.pdf",
+    //     "cat": [
+    //         "College"
+    //     ],
+    //     "issuing_auth": [
+    //         "Thapar Institute of Engineering and Technology"
+    //     ],
+    //     "date": "2021-03-01",
+    //     "cert_id": "EVd5tlDauk",
+    //     "other_keywords":[]
+    // },
+    // {
+    //     "name":"OPERATING SYSTEMS-UCS303-2021ODDSEM",
+    //     "file_name": "UCS303_Operating_Systems_Completion_Certificate.pdf",
+    //     "cat": [
+    //         "College"
+    //     ],
+    //     "issuing_auth": [
+    //         "Thapar Institute of Engineering and Technology"
+    //     ],
+    //     "date": "2021-03-02",
+    //     "cert_id": "lIJnowShmF",
+    //     "other_keywords":[]
+    // },
+    // {
+    //     "name":"PRACTICAL COMPUTING-UCS311-2021ODDSEM",
+    //     "file_name": "UCS311_Practical_Computing_Completion_Certificate.pdf",
+    //     "cat": [
+    //         "College"
+    //     ],
+    //     "issuing_auth": [
+    //         "Thapar Institute of Engineering and Technology"
+    //     ],
+    //     "date": "2021-03-02",
+    //     "cert_id": "fKBydjzUSE",
+    //     "other_keywords":[]
+    // },
+    // {
+    //     "name":"DISCRETE MATHEMATICAL STRUCTURES-UCS405-2021ODDSEM",
+    //     "file_name": "UCS405_Discrete_Mathematical_Structures_Completion_Certificate.pdf",
+    //     "cat": [
+    //         "College"
+    //     ],
+    //     "issuing_auth": [
+    //         "Thapar Institute of Engineering and Technology"
+    //     ],
+    //     "date": "2021-03-02",
+    //     "cert_id": "gCnKAnFPMi",
+    //     "other_keywords":[]
+    // },
+    // {
+    //     "name":"ENGINEERING DESIGN PROJECT-I-UTA013-2021ODDSEM",
+    //     "file_name": "UTA013_Engineering_Design_Project_I_Completion_Certificate.pdf",
+    //     "cat": [
+    //         "College"
+    //     ],
+    //     "issuing_auth": [
+    //         "Thapar Institute of Engineering and Technology"
+    //     ],
+    //     "date": "2021-03-03",
+    //     "cert_id": "1xrtg8lQmT",
+    //     "other_keywords":[]
+    // },
+    {
+        "name":"Career Essentials in Generative AI",
+        "file_name": "Career Essentials in Generative AI by Microsoft and LinkedIn.pdf",
+        "cat": [
+            "Linkedin Learning",
+            "Generative AI"
+        ],
+        "issuing_auth": [
+            "Microsoft",
+            "LinkedIn Learning"
+        ],
+        "date": "2024-06-17",
+        "cert_id": " 2dfa3b0a230fefcc1c470164b1919e4265d98a97477fcd7c5bed0d37505629ee",
+        "other_keywords":[]
+    },
+    {
+        "name":"Accenture Development & Advanced Engineering Job Simulation",
+        "file_name": "Accenture North America Coding Development & Advanced Engineering Job Simulation.pdf",
+        "cat": [
+            "Virtual Internship"
+        ],
+        "issuing_auth": [
+            "The Forage",
+            "Accenture"
+        ],
+        "date": "2024-06-13",
+        "cert_id": "ZxiNmHBGWokfHSwzF",
+        "other_keywords":[]
+    },
+    {
+        "name":"OCI Generative AI Certification",
+        "file_name": "OCI Generative AI Certification.pdf",
+        "cat": [
+            "Online Course",
+            "Generative AI"
+        ],
+        "issuing_auth": [
+            "Oracle University"
+        ],
+        "date": "2024-07-23",
+        "cert_id": "312795178OCI2024GAIOCP",
+        "other_keywords":[]
+    }
+];
+
+export default certificates_data;
--- src/pages/CertificatesSection/CertificatesSection.js
+++ src/pages/CertificatesSection/CertificatesSection.js
@@ -1,11 +1,129 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './CertificatesSection.css';
 import SectionHeading from "../../components/SectionHeading/SectionHeading";
+import certificates_data from "../../assets/data/certificates_data";
+
+import { Document, Page, pdfjs } from 'react-pdf';
+import ReactCardFlip from 'react-card-flip';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 
 const CertificatesSection = () => {
+    
+    function sortCertificatesByDate(certificates) {
+        return certificates.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+
     return (
         <div id="certificates-section">
             <SectionHeading section_name="CERTIFICATES"/>
+            <div id="certificates">
+                {
+                    sortCertificatesByDate(certificates_data).map((certificate) => 
+                        <Certificate certificate={certificate}/>
+                    )
+                }
+            </div>
+        </div>
+    );
+}
+
+const Certificate = ({certificate}) => {
+
+    const [pdfFile, setPdfFile] = useState(null);
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    useEffect(() => {
+        const loadPDF = async (certi) => {
+            const module = await import(`../../assets/pdfs/${certi}`);
+            setPdfFile(module.default);
+        };
+
+        loadPDF(certificate.file_name);
+    }, [certificate.file_name]);
+    
+    return (
+        <ReactCardFlip 
+            flipDirection="horizontal" 
+            isFlipped={isFlipped}
+        >
+            <CertificateFront 
+                certificate={certificate} 
+                pdfFile={pdfFile} 
+                isFlipped={isFlipped}
+                setIsFlipped={setIsFlipped}/>
+            <CertificateBack 
+                certificate={certificate} 
+                pdfFile={pdfFile} 
+                isFlipped={isFlipped}
+                setIsFlipped={setIsFlipped}/>
+        </ReactCardFlip>
+    );
+
+}
+
+const CertificateFront = ({certificate, pdfFile, isFlipped, setIsFlipped}) => {
+    
+    function formatDate(dateString) {
+        const options = { year: 'numeric', month: 'short', day: '2-digit' };
+        return new Date(dateString).toLocaleDateString('en-US', options);
+    }
+
+    return (
+        <div className="certificate-front">
+            <div className="certificate-title">{certificate.name}</div>
+            {pdfFile ? (
+                <Document file={pdfFile}>
+                    <Page pageNumber={1} renderAnnotationLayer={false} renderTextLayer={false} width="280"/>
+                </Document>
+            ) : (
+                <div>Loading PDF...</div>
+            )}
+            <div className="certificate-date">
+                {formatDate(certificate.date)}
+            </div>
+            <div className="certificate-icons">
+                <FlipButton isFlipped={isFlipped} setIsFlipped={setIsFlipped}/>
+            </div>
+        </div>
+    );
+}
+
+const CertificateBack = ({certificate, pdfFile, isFlipped, setIsFlipped}) => {
+    
+    const categories = certificate.cat;
+    
+    return (
+        <div className="certificate-back">
+            <div className='certificate-categories'>
+                {categories.map(category => (
+                    <span key={category} className='certificate-category'>{category}</span>
+                ))}
+            </div>
+            <div className='certificate-issuing-auth'>
+                Issued By: {certificate.issuing_auth.map(authority => (
+                    <span key={authority}>{authority} </span>
+                ))}
+            </div>
+            <div className="certificate-icons">
+                <div className='open-button'>
+                    <a href={pdfFile} target="_blank" rel="noreferrer">
+                        Open 
+                        {/* <img src={share} height='25px' width='25px' alt='' /> */}
+                    </a>
+                </div>
+                <FlipButton isFlipped={isFlipped} setIsFlipped={setIsFlipped}/>
+            </div>
+        </div>
+    );
+}
+
+const FlipButton = ({isFlipped, setIsFlipped}) => {
+    return (
+        <div className="flip-button">
+            <div onClick={() => setIsFlipped(!isFlipped)}>
+                {isFlipped? "Less": "More"} Details
+            </div>
         </div>
     );
 }
