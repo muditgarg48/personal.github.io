f94431415f0aec98769f095387bb1f36af3f54e6
muditgarg48 <muditgarg48@gmail.com>
2024-08-26 16:56:15 +0530

    Ignorables

--- src/pages/ChatbotSection/ChatbotSection.js
+++ src/pages/ChatbotSection/ChatbotSection.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react'
 // import React, {useState, useEffect} from 'react'
 import './ChatbotSection.css'
 import axios from 'axios';
+import { TypeAnimation } from "react-type-animation";
 
 import SectionHeading from '../../components/SectionHeading/SectionHeading';
 
@@ -166,7 +167,6 @@ function ChatbotSection() {
         {
           chatActive === true?
             <ChatHistory chatHistory={chatHistory} alfredEndRef={alfredEndRef}/>:
-            // <ChatHistory chatHistory={chatHistory}/>:
             <ChatInactive/>
         }
         <div style={{display: "flex", justifyContent: "center"}}>
